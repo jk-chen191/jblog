bf53bcf5d71140fd88179dc9e550dc7c94b3d663
jewelknife <jewelknife@qq.com>
2014-11-14 08:08:01 +0800

    aaa

--- jblog-springmvc/database/schema.sql
+++ jblog-springmvc/database/schema.sql
@@ -12,4 +12,16 @@ CREATE TABLE user (
   `last_visit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY (email)
-) ENGINE=MyISAM AUTO_INCREMENT=1440 DEFAULT CHARSET=latin1
+) ENGINE=Innodb AUTO_INCREMENT=1440 DEFAULT CHARSET=latin1
+
+
+CREATE TABLE post (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `content` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `created_by` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_by` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=Innodb AUTO_INCREMENT=1440 DEFAULT CHARSET=latin1
--- jblog-springmvc/src/main/java/org/jewelknife/jblog/mvc/controller/LoginController.java
+++ jblog-springmvc/src/main/java/org/jewelknife/jblog/mvc/controller/LoginController.java
@@ -1,12 +1,19 @@
 package org.jewelknife.jblog.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by chen_yingbo on 11/13/14.
  */
 @Controller
 public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public void getLogin() {
+    }
 
 
 
