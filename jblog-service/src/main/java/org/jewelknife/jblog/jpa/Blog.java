da33d533375d4cc19e7ec549505275173d788808
jewelknife <jewelknife@qq.com>
2014-11-19 20:03:49 +0800

    update

---
+++ jblog-service/src/main/java/org/jewelknife/jblog/jpa/Blog.java
@@ -0,0 +1,7 @@
+package org.jewelknife.jblog.jpa;
+
+/**
+ * Created by chen_yingbo on 11/19/14.
+ */
+public class Blog {
+}
---
+++ jblog-springmvc/src/main/java/org/jewelknife/jblog/mvc/controller/BlogController.java
@@ -0,0 +1,7 @@
+package org.jewelknife.jblog.mvc.controller;
+
+/**
+ * Created by chen_yingbo on 11/19/14.
+ */
+public class BlogController {
+}
